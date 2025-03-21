6eb1a7d56a66c78e30867b5d0df9a0d8e2dc7feb
yasmine alaya <alayayesmine12@gmail.com>
2025-03-21 22:16:43 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom"; // Ajoutez cette importation
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter> 
+      <CssBaseline />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
