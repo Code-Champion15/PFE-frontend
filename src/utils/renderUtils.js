d25c14609c18920a64d9eb042b9472672f881cae
yasmine alaya <alayayesmine12@gmail.com>
2025-03-28 17:22:52 +0100

    update renderUtils

--- src/utils/renderUtils.js
+++ src/utils/renderUtils.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { Container, Typography, Button, Box, Card, CardMedia, CardContent, Accordion, AccordionSummary, AccordionDetails } from "@mui/material"; 
-
-const componentsMap = {
-  container: Container,
-  typography: Typography,
-  button: Button,
-  box: Box,
-  card: Card,
-  cardmedia: CardMedia,
-  cardcontent: CardContent,
-  accordion: Accordion,
-  accordionsummary:AccordionSummary,
-  accordiondetails: AccordionDetails,
-};
+import EditableWrapper from "../components/EditableWrapper";
 
 
+//pour la creation des pages
 export const createElementFromJson = (element, key) => {
   if (!element || !element.type) return null;
 
-  const Component = componentsMap[element.type.toLowerCase()] || "div";// Utiliser un div par défaut si non trouvé
+  const componentsMap = {
+    container: Container,
+    typography: Typography,
+    button: Button,
+    box: Box,
+    card: Card,
+    cardmedia: CardMedia,
+    cardcontent: CardContent,
+    accordion: Accordion,
+    accordionsummary:AccordionSummary,
+    accordiondetails: AccordionDetails,
+  };
+
+  const Component = componentsMap[element.type.toLowerCase()] || "div";
 
   let children = element.children;
   if (typeof children === "string") {
@@ -32,5 +34,48 @@ export const createElementFromJson = (element, key) => {
       {children}
      
     </Component>
+  );
+};
+
+
+//pour le mode edit : les elements enveloppés
+export const createEditableElementFromJson = (element, path = "0", onSelect) => {
+  if (!element || !element.type) return null;
+
+  const componentsMap = {
+    container: Container,
+    typography: Typography,
+    button: Button,
+    box: Box,
+    card: Card,
+    cardmedia: CardMedia,
+    cardcontent: CardContent,
+    accordion: Accordion,
+    accordionsummary: AccordionSummary,
+    accordiondetails: AccordionDetails,
+  };
+
+  const Component = componentsMap[element.type.toLowerCase()] || "div";
+  
+  // Récupération des enfants avec parcours récursif
+  let children = null;
+  if (typeof element.children === "string") {
+    children = element.children;
+  } else if (Array.isArray(element.children)) {
+    children = element.children.map((child, index) =>
+      createEditableElementFromJson(child, `${path}-${index}`, onSelect)
+    );
+  }
+  // Envelopper l'élément pour qu'il soit cliquable
+  return (
+    <EditableWrapper onClick={(e) => { 
+      e.stopPropagation(); 
+      onSelect && onSelect(path);
+    }}
+    >
+      <Component {...element.props}>
+        {children}
+      </Component>
+    </EditableWrapper>
   );
 };
